ef24f6cc7f615c6d3bd1e31bffa6f9778f049b19
Evgeny Prilepin <esp.home@gmail.com>
2025-11-13 11:33:52 +0000

    test: add tests for runners

---
+++ tests/test_runners.py
@@ -0,0 +1,131 @@
+import sys
+from uuid import uuid4
+
+import pytest
+
+from filen.runners import (
+    AsyncInterpreterRunner,
+    AsyncProcessRunner,
+    AsyncThreadRunner,
+    InterpreterRunner,
+    ProcessRunner,
+    ThreadRunner,
+)
+
+
+def task(n, tid):
+    return n, tid
+
+
+async def atask(n, tid):
+    return n, tid
+
+
+def error(n):
+    raise ValueError(n)
+
+
+def maybe_skip_interpreter_tests(runner_cls):
+    if issubclass(runner_cls, InterpreterRunner) and sys.version_info < (3, 14):
+        pytest.skip('InterpreterRunner works only in Python >= 3.14')
+
+    elif issubclass(runner_cls, AsyncInterpreterRunner) and sys.version_info < (3, 13):
+        pytest.skip('AsyncInterpreterRunner works only in Python >= 3.13')
+
+
+@pytest.mark.parametrize('runner_cls', [ThreadRunner, ProcessRunner, InterpreterRunner])
+def test_runner_task_group(runner_cls):
+    maybe_skip_interpreter_tests(runner_cls)
+
+    task_ids = [1, 2, 't1', 't2', uuid4(), uuid4()]
+
+    with runner_cls() as runner:
+        r = set()
+
+        with runner.task_group() as gr:
+            for n, tid in enumerate(task_ids):
+                r.add((tid, (n, tid)))
+                gr.add_task(tid, task, n, tid=tid)
+
+        assert set(gr.results.items()) == r
+
+
+@pytest.mark.parametrize('runner_cls', [ThreadRunner, ProcessRunner, InterpreterRunner])
+def test_runner_task_group_error(runner_cls):
+    maybe_skip_interpreter_tests(runner_cls)
+
+    with runner_cls() as runner:
+        with runner.task_group(return_exceptions=True) as gr:
+            gr.add_task('t1', task, 0, 't1')
+
+            for n in range(1, 5):
+                gr.add_task(n, error, n)
+
+        for tid, r in gr.results.items():
+            if isinstance(tid, int):
+                assert isinstance(r, ValueError)
+                assert r.args[0] == tid
+            else:
+                assert r == (0, tid)
+
+        with pytest.raises(ExceptionGroup):
+            with runner.task_group() as gr:
+                gr.add_task(1, task, 0, 1)
+                gr.add_task(2, error, 1)
+                gr.add_task(3, error, 2)
+
+        assert len(gr.results) == 0
+
+
+@pytest.mark.parametrize('runner_cls', [AsyncThreadRunner, AsyncProcessRunner, AsyncInterpreterRunner])
+async def test_async_runner_run_sync(runner_cls):
+    maybe_skip_interpreter_tests(runner_cls)
+
+    runner = runner_cls()
+
+    res = await runner.run_sync(task, 1, 1)
+    assert res == (1, 1)
+
+    res = await runner.run_sync(task, 2, 2)
+    assert res == (2, 2)
+
+
+@pytest.mark.parametrize('runner_cls', [AsyncThreadRunner, AsyncProcessRunner, AsyncInterpreterRunner])
+async def test_async_runner_task_group(runner_cls):
+    maybe_skip_interpreter_tests(runner_cls)
+
+    task_ids1 = [1, 2, 't1', 't2', uuid4(), uuid4()]
+    task_ids2 = [3, 4, 't3', 't4', uuid4(), uuid4()]
+    r = set()
+
+    runner = runner_cls()
+
+    async with runner.task_group() as gr:
+        for n, tid in enumerate(task_ids1):
+            r.add((tid, (n, tid)))
+            gr.add_task(tid, task, n, tid=tid)
+
+        for n, tid in enumerate(task_ids2):
+            r.add((tid, (n, tid)))
+            gr.add_task(tid, atask, n, tid=tid)
+
+    assert set(gr.results.items()) == r
+
+
+@pytest.mark.parametrize('runner_cls', [AsyncThreadRunner, AsyncProcessRunner, AsyncInterpreterRunner])
+async def test_async_runner_task_group_error(runner_cls):
+    maybe_skip_interpreter_tests(runner_cls)
+
+    runner = runner_cls()
+
+    with pytest.raises(ExceptionGroup):
+        async with runner.task_group() as gr:
+            gr.add_task(1, task, 1, tid=1)
+            gr.add_task(2, atask, 2, tid=2)
+            gr.add_task(3, error, 3)
+
+    with pytest.raises(NotImplementedError):
+        async with runner.task_group(return_exceptions=True) as gr:
+            gr.add_task(3, error, 3)
+
+    assert len(gr.results) == 0
