335a85f61c6952ff240b4a2def884658d772638f
Evgeny Prilepin <esp.home@gmail.com>
2025-11-15 01:15:54 +0000

    test(crypto): add tests for metadata decryption and encryption version errors

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -1,3 +1,5 @@
+import pytest
+
 from filen.crypto import (
     MetadataEncryptionVersion,
     current_metadata_cipher,
@@ -6,6 +8,7 @@ from filen.crypto import (
     encrypt_metadata,
     metadata_ciphers,
 )
+from filen.errors import MetadataDecryptError, MetadataEncryptionVersionError
 
 
 def test_derive_password_and_master_key():
@@ -65,3 +68,37 @@ def test_encrypt_decrypt_metadata():
     assert decrypt_metadata(encrypt_metadata(metadata, key1), [key1, key2, key3]) == metadata
     assert decrypt_metadata(encrypt_metadata(metadata, key2), [key1, key2, key3]) == metadata
     assert decrypt_metadata(encrypt_metadata(metadata, key3), [key1, key2, key3]) == metadata
+
+
+def test_decrypt_metadata_error():
+    metadata = (
+        'Filen uses symmetric AES-256-GCM cryptography. '
+        'We differentiate between two basic encryption concerns: '
+        'Metadata encryption is our term for any small strings, like file metadata or directory names, '
+        'that need to be encrypted. Data encryption means encryption of binary file content.'
+    )
+
+    key1 = 'ef8bcd0f76c70767e533b2695e134b75fcf00baa2000aacf57242186bde125ef'
+    key2 = '7ef190b14d40eee8ac991d2bab40c613877ee1f22f225c371e22fc5321422550'
+    key3 = '1828d1cba88e28377a3d9c8f64e3aad36a0287fb5f9ad3485f9922b071827aa0'
+
+    metadata_e = encrypt_metadata(metadata, key1)
+
+    with pytest.raises(MetadataDecryptError):
+        _ = decrypt_metadata(metadata_e, key2)
+
+    with pytest.raises(MetadataDecryptError):
+        _ = decrypt_metadata(metadata_e, [key2, key3])
+
+
+def test_decrypt_metadata_encryption_version_error():
+    metadata_e = '00028964607db67225e338612059776447715d37586eb484766b8ff500b0b918fd44f1f643970767e533b2695e134b75'
+
+    key1 = 'c438c484766b8ff500b0b918fd44f1f643929f7656a648f6b3dd76aea56c121b'
+    key2 = 'd899ab9d9032c49ff39428964607db67225e338612059776447715d37586eba3'
+
+    with pytest.raises(MetadataEncryptionVersionError):
+        _ = decrypt_metadata(metadata_e, key1)
+
+    with pytest.raises(MetadataEncryptionVersionError):
+        _ = decrypt_metadata(metadata_e, [key1, key2])
