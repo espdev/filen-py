946611b5a6b703e90183ce970c4f00093a011f19
Evgeny Prilepin <esp.home@gmail.com>
2025-11-21 22:39:26 +0000

    refactor(api): reduce num modules

--- src/filen/api/__init__.py
+++ src/filen/api/__init__.py
@@ -5,7 +5,7 @@ APIs implement functionality for interacting with Filen API on low-level data mo
 """
 
 from ._base import APIBase, APIEndpoint, APINamespaceBase, AsyncAPIBase, AsyncAPINamespaceBase, api
-from ._filen import AsyncFilenAPI, FilenAPI
+from .v3 import APIv3, AsyncAPIv3
 
 __all__ = [
     'api',
@@ -17,3 +17,15 @@ __all__ = [
     'FilenAPI',
     'AsyncFilenAPI',
 ]
+
+
+class FilenAPI(APINamespaceBase):
+    """Sync Filen API namespace"""
+
+    v3: APIv3 = api(APIv3)
+
+
+class AsyncFilenAPI(AsyncAPINamespaceBase):
+    """Async Filen API namespace"""
+
+    v3: AsyncAPIv3 = api(AsyncAPIv3)
