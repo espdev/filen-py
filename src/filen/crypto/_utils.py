980bed740eb76665caf3fd29641cf3192eb4eefe
Evgeny Prilepin <esp.home@gmail.com>
2025-11-28 13:24:16 +0000

    refactor: update docstring

--- src/filen/crypto/_utils.py
+++ src/filen/crypto/_utils.py
@@ -18,5 +18,8 @@ def generate_random_string(length: int) -> str:
 
     This guarantees that the length of the string will be equal to the size in bytes,
     but there is a loss of entropy.
+
+    This is used in metadata encryption v2 and public link v2.
     """
+
     return ''.join(B64_CHARSET[b % B64_CHARSET_LENGTH] for b in token_bytes(length))
