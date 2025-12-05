b0879144cc8e0075b33de1144a0105ac353b4319
Evgeny Prilepin <esp.home@gmail.com>
2025-12-05 23:42:40 +0000

    refactor(api): a slight refactoring

--- src/filen/api/v3/_download.py
+++ src/filen/api/v3/_download.py
@@ -9,22 +9,22 @@ from .._base import APIBase, AsyncAPIBase
 class FileDownloadMixIn:
     _context: Context
 
-    def _get_chunk_url(self, uuid: UUID, bucket: str, region: str, chunk: int) -> str:
+    def _download_chunk_url(self, uuid: UUID, bucket: str, region: str, index: int) -> str:
         base_url = self._context.get_egest_url().rstrip('/')
-        return f'{base_url}/{region}/{bucket}/{uuid}/{chunk}'
+        return f'{base_url}/{region}/{bucket}/{uuid}/{index}'
 
 
 class FileDownloadAPI(APIBase, FileDownloadMixIn):
     """API for downloading files from the storage"""
 
-    def chunk(self, uuid: UUID, bucket: str, region: str, chunk: int) -> bytes:
+    def chunk(self, uuid: UUID, bucket: str, region: str, index: int) -> bytes:
         """Download a file encrypted chunk"""
 
-        chunk_url = self._get_chunk_url(uuid, bucket, region, chunk)
-        headers = self._ensure_api_key(use_api_key=True, url=chunk_url)
+        url = self._download_chunk_url(uuid, bucket, region, index)
+        headers = self._ensure_api_key(use_api_key=True, url=url)
 
         with self._request_error_handler:
-            with self._http_client.stream('GET', chunk_url, headers=headers) as resp:
+            with self._http_client.stream('GET', url, headers=headers) as resp:
                 resp.raise_for_status()
                 return b''.join(resp.iter_raw(chunk_size=DOWNLOAD_STREAM_CHUNK_SIZE))
 
@@ -32,14 +32,14 @@ class FileDownloadAPI(APIBase, FileDownloadMixIn):
 class AsyncFileDownloadAPI(AsyncAPIBase, FileDownloadMixIn):
     """Async API for downloading files from the storage"""
 
-    async def chunk(self, uuid: UUID, bucket: str, region: str, chunk: int) -> bytes:
+    async def chunk(self, uuid: UUID, bucket: str, region: str, index: int) -> bytes:
         """Download a file encrypted chunk"""
 
-        chunk_url = self._get_chunk_url(uuid, bucket, region, chunk)
-        headers = self._ensure_api_key(use_api_key=True, url=chunk_url)
+        url = self._download_chunk_url(uuid, bucket, region, index)
+        headers = self._ensure_api_key(use_api_key=True, url=url)
 
         with self._request_error_handler:
-            async with self._http_client.stream('GET', chunk_url, headers=headers) as resp:
+            async with self._http_client.stream('GET', url, headers=headers) as resp:
                 resp.raise_for_status()
                 buffer = bytearray()
                 async for c in resp.aiter_raw(chunk_size=DOWNLOAD_STREAM_CHUNK_SIZE):
